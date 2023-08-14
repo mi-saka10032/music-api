31c183011e625b4703e901563a8c7caf89d3f29e
misakayu <misakayu@tencent.com>
2023-08-14 19:03:13 +0800

    更新登录密码校验规则

--- dto/LoginDTO.ts
+++ dto/LoginDTO.ts
@@ -1,6 +1,10 @@
 import { Rule, RuleType } from '@midwayjs/validate';
 import { ApiProperty } from '@midwayjs/swagger';
 
+/** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
+export const REGEXP_PWD =
+  /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)]|[()])+$)(?!^.*[\u4E00-\u9FA5].*$)([^(0-9a-zA-Z)]|[()]|[a-z]|[A-Z]|[0-9]){8,18}$/;
+
 /**
  * 登录DTO
  */
@@ -9,7 +13,7 @@ export class LoginDTO {
   @Rule(RuleType.string().required().min(5).max(20))
   username: string;
 
-  @ApiProperty({ type: String, example: '123456', description: '密码' })
-  @Rule(RuleType.string().required().min(5).max(20))
+  @ApiProperty({ type: String, example: '123456abc', description: '密码' })
+  @Rule(RuleType.string().required().pattern(REGEXP_PWD, '密码格式应为8-18位数字、字母、符号的任意两种组合'))
   password: string;
 }

8677bfcac9856b9ae09c25b740bf88e7f15d7a1c
misakayu <misakayu@tencent.com>
2023-08-15 10:02:48 +0800

    调整Login结构

--- dto/LoginDTO.ts
+++ dto/LoginDTO.ts
@@ -1,9 +1,6 @@
 import { Rule, RuleType } from '@midwayjs/validate';
 import { ApiProperty } from '@midwayjs/swagger';
-
-/** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
-export const REGEXP_PWD =
-  /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)]|[()])+$)(?!^.*[\u4E00-\u9FA5].*$)([^(0-9a-zA-Z)]|[()]|[a-z]|[A-Z]|[0-9]){8,18}$/;
+import { REGEXP_PWD } from "../rule/LoginRule";
 
 /**
  * 登录DTO

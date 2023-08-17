811b9a8b6ddc6b0ed03f4d720da8d41a9dcf40bd
misakayu <misakayu@tencent.com>
2023-08-17 17:24:06 +0800

    增加PageVOClass

--- vo/PageVO.ts
+++ vo/PageVO.ts
@@ -6,11 +6,21 @@ import { ApiProperty, Type } from '@midwayjs/swagger';
 export function PageVO<T extends Type>(list: T): any {
   class WrapperPageVO {
     @ApiProperty({ type: [list], description: '列表' })
-    list: T[];
+    list: Array<T>;
 
     @ApiProperty({ type: Number, example: 0, description: '总数' })
     total: number;
   }
 
   return WrapperPageVO;
+}
+
+/**
+ * 正规class形式的PageVO，方便前端Pick类型
+ * 不含ApiProperty 不适用Swagger
+ */
+export class PageVOClass<T> {
+  list: Array<T>;
+
+  total: number;
 }

0ca32b913484ac5127c3e9d793a469f8cba12489
misaka10032 <misaka10032@aliyun.com>
2023-08-27 00:54:23 +0800

    feat: 补充歌曲进度enum的文本对象

--- code/SocketSongEnum.ts
+++ code/SocketSongEnum.ts
@@ -1,12 +1,19 @@
 /** 歌曲信息的Socket枚举值 */
 export enum SocketSongEnum {
-  Start, // 上传完成，解析开始
-  BasicAnalysis, // 基础信息解析完成
-  OSS, // OSS上传完成
-  DetailAnalysis, // 详细信息解析完成
+  Start,
+  BasicAnalysis,
+  OSS,
+  DetailAnalysis
 }
 
 export interface SocketSongResponse {
   originalName: string;
   status: SocketSongEnum;
 }
+
+export const SocketSongText = {
+  0: "上传完成，解析开始",
+  1: "基础信息解析完成",
+  2: "OSS上传完成",
+  3: "详细信息解析完成"
+};
